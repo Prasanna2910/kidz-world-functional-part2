6e9bd09ae7884558e5e390f7e7f99c9e836dfd80
Prasanna V <you@example.com>
2024-01-31 10:33:58 +0530

    coded

--- app.js
+++ app.js
@@ -1,89 +1,89 @@
-var cartValue = document.getElementById("cart-value");
-var cartButton = document.getElementById("cart");
+var cartValue = document.getElementById('cart-value');
+var cartButton = document.getElementById('cart');
 
-var addButtons = document.getElementsByClassName("button");
+var addButtons = document.getElementsByClassName('button');
 
 var items = [
   {
-    name: "This was our pact",
+    name: 'This was our pact',
     quantity: 0,
     dollars: 7,
     cents: 49,
   },
   {
-    name: "The famous five",
+    name: 'The famous five',
     quantity: 0,
     dollars: 4,
     cents: 59,
   },
   {
-    name: "Matilda",
+    name: 'Matilda',
     quantity: 0,
     dollars: 6,
     cents: 80,
   },
   {
-    name: "Harry Potter",
+    name: 'Harry Potter',
     quantity: 0,
     dollars: 10,
     cents: 0,
   },
   {
-    name: "For Young Readers",
+    name: 'For Young Readers',
     quantity: 0,
     dollars: 7,
     cents: 29,
   },
   {
-    name: "Wimpy Kid - DIY",
+    name: 'Wimpy Kid - DIY',
     quantity: 0,
     dollars: 4,
     cents: 99,
   },
   {
-    name: "Dart Board",
+    name: 'Dart Board',
     quantity: 0,
     dollars: 17,
     cents: 49,
   },
   {
-    name: "Connect Four",
+    name: 'Connect Four',
     quantity: 0,
     dollars: 19,
     cents: 99,
   },
   {
-    name: "Jenga",
+    name: 'Jenga',
     quantity: 0,
     dollars: 20,
     cents: 99,
   },
   {
-    name: "Monopoly",
+    name: 'Monopoly',
     quantity: 0,
     dollars: 19,
     cents: 49,
   },
   {
-    name: "Bookmarks",
+    name: 'Bookmarks',
     quantity: 0,
     dollars: 12,
     cents: 49,
   },
   {
-    name: "Birthday Card",
+    name: 'Birthday Card',
     quantity: 0,
     dollars: 12,
     cents: 49,
   },
   {
-    name: "Stuffed toys",
+    name: 'Stuffed toys',
     quantity: 0,
     dollars: 15,
     cents: 99,
   },
   {
-    name: "Dream catcher drawing",
+    name: 'Dream catcher drawing',
     quantity: 0,
     dollars: 18,
     cents: 49,
@@ -120,23 +120,23 @@ function updatePrice() {
   finalCents = totalPriceInCents % 100;
 }
 
-
 cartButton.onclick = () => {
   updatePrice();
-
 
   for (let index = 0; index < items.length; index++) {
     if (items[index].quantity != 0) {
       console.log(
-        "Item name: " +
+        'Item name: ' +
           items[index].name +
-          " - Quantity: " +
+          ' - Quantity: ' +
           items[index].quantity
       );
     }
   }
-
-  console.log(
-    "The total amount is " + finalDollars + "$ and " + finalCents + " cents"
+  let message =
+    'The total amount is ' + finalDollars + '$ and ' + finalCents + ' cents';
+  console.log(message);
+  window.open(
+    'https://wa.me/+919481574558?text=' + encodeURIComponent(message)
   );
 };
